13238917652a2ac9f2e6c7fce05b9d170bf002b4
Hitalo Siqueira <hsiqueira10@gmail.com>
2018-07-26 21:03:35 -0300

    first commit

---
+++ cards/lib/cards.ex
@@ -0,0 +1,72 @@
+defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    create a list of cards as a deck
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+    Determines wether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Hearts")
+      true
+
+  """
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  @doc """
+    Divides a deck of cards into a hand
+    the `hand_size` argument is how many cards should be in the hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, _} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Hearts"]
+
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} ->
+        :erlang.binary_to_term(binary)
+      {:error, _} ->
+        "file not found"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |>Cards.shuffle
+    |>Cards.deal(hand_size) #o resultado de uma função eh automaticamente injetado como o primeiro parametro da próxima
+  end
+
+end
---
+++ teste/lib/teste.ex
@@ -0,0 +1,19 @@
+defmodule Teste do
+  @moduledoc """
+  Documentation for Teste.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Teste.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+end
